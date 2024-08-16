3c2f8a00789ea0544b17c1c1fb07543925f9b8fc
rebecalasconi <rebecavoicilas@yahoo.com>
2024-08-16 23:10:14 +1400

    ex 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,32 @@
+"use strict";
+
+import VimeoPlayer from "@vimeo/player";
+
+const onPlay = function() {
+    let currentTime = player.currentTime();
+    let startTime = 0;
+    startTime = currentTime;
+    // data is an object containing properties specific to that event
+};
+
+player.on('timeupdate', function(data) {
+   let time = data.seconds;
+   let timeString = JSON.stringify(time);
+      localStorage.setItem('videoplayer-current-time', time);
+   
+});
+
+
+let storageTime = localStorage.getItem('videoplayer-current-time');
+let javasTime = JSON.parse(storageTime);
+
+player.setCurrentTime(javasTime).then(function(seconds) {
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            break;
+        default:
+            break;
+    }
+});
 
